4e45b5151511acd98fc4b416f15b92a0dea752bf
kyusque <kawashimayusuke113@gmail.com>
2018-05-30 14:34:44 +0900

    make a dance switcher

---
+++ Assets/Scripts/danceSwitch.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class danceSwitch : MonoBehaviour {
+
+    [SerializeField]
+    int n, b;
+    Animator anm;
+
+    // Use this for initialization
+    void Start()
+    {
+        anm = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anm.SetBool("Next", false);
+        if (n > 0)
+        {
+            anm.SetBool("Next", true);
+            n -= 1;
+        }
+        anm.SetBool("Back", false);
+        if (b > 0)
+        {
+            anm.SetBool("Back", true);
+
+            b -= 1;
+        }
+
+    }
+}
